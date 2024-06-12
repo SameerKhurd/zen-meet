386b42229dde2649b13b14303b03a3815be16213
Sameer Khurd <sameerkhurd@gmail.com>
2024-06-12 19:31:52 +0530

    Updated configurations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,13 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyCjBDWG4xGDvy8Xxpc1ivGD7V90XdWbvWQ",
-    authDomain: "jalwa-instagram-clone.firebaseapp.com",
-    databaseURL:
-      "https://jalwa-instagram-clone-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "jalwa-instagram-clone",
-    storageBucket: "jalwa-instagram-clone.appspot.com",
-    messagingSenderId: "421746507243",
-    appId: "1:421746507243:web:ff7ce8c5445d92221a6dae",
-    measurementId: "G-SRXET4FE7K",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
   },
 };
 
